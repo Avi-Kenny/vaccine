bde0a4beb7fe474c55e5f44d1b42437eb0709dd6
Avi Kenny <avikenny@uw.edu>
2023-06-16 17:15:41 -0700

    Added sample dataset

--- R/vaccine-package.R
+++ R/vaccine-package.R
@@ -1,2 +1,18 @@
 #' @keywords internal
 "_PACKAGE"
+
+#' Data from the HVTN 505 clinical trial
+#'
+#' A dataset containing the prices and attributes of almost 54,000 diamonds
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \itemize{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#' }
+#' @source \url{https://atlas.scharp.org/cpas/project/HVTN%20Public%20Data/HVTN%20505/begin.view}
+#' @docType data
+#' @keywords datasets
+#' @name hvtn505
+#' @usage data(hvtn505)
+"hvtn505"
