a32598bf1c4148a8b6d62b58051814c97e83bda2
Avi Kenny <avikenny@uw.edu>
2023-06-19 22:07:59 -0700

    Added several summary stats

--- R/summary_stats.R
+++ R/summary_stats.R
@@ -13,33 +13,71 @@
 #' @export
 summary_stats <- function(dat, quietly=F) {
 
-  # !!!!! Build this out
-
   if (!methods::is(dat,"dat_vaccine")) {
     stop(paste0("`dat` must be an object of class 'dat_vaccine' returned by lo",
                 "ad_data()."))
   }
 
-  # Checks based on identical(dat$v,list()) and identical(dat$p,list())
+  # !!!!! Checks based on identical(dat$v,list()) and identical(dat$p,list())
+
+  num_ph1_subj_v <- length(dat$v$delta)
+  num_ph1_subj_p <- length(dat$p$delta)
+  num_ph2_subj_v <- sum(dat$v$z)
+  num_ph2_subj_p <- sum(dat$p$z)
+  num_ph1_events_v <- sum(dat$v$delta==1)
+  num_ph1_events_p <- sum(dat$p$delta==1)
+  num_ph2_events_v <- sum(dat$v$z==1 & dat$v$delta==1)
+  num_ph2_events_p <- sum(dat$p$z==1 & dat$p$delta==1)
 
   ss <- list(
-    "num_ph1_events_v" = sum(dat$v$delta==1),
-    "num_ph1_events_p" = sum(dat$p$delta==1),
-    "num_ph2_events_v" = sum(dat$v$z==1 & dat$v$delta==1),
-    "num_ph2_events_p" = sum(dat$p$z==1 & dat$p$delta==1)
+    "num_ph1_subj_v" = num_ph1_subj_v,
+    "num_ph1_subj_p" = num_ph1_subj_p,
+    "num_ph2_subj_v" = num_ph2_subj_v,
+    "num_ph2_subj_p" = num_ph2_subj_p,
+    "num_ph1_events_v" = num_ph1_events_v,
+    "num_ph1_events_p" = num_ph1_events_p,
+    "num_ph2_events_v" = num_ph2_events_v,
+    "num_ph2_events_p" = num_ph2_events_p,
+    "prop_ph1_events_v" = round(num_ph1_events_v/num_ph1_subj_v, 5),
+    "prop_ph1_events_p" = round(num_ph1_events_p/num_ph1_subj_p, 5),
+    "prop_ph2_events_v" = round(num_ph2_events_v/num_ph2_subj_v, 5),
+    "prop_ph2_events_p" = round(num_ph2_events_p/num_ph2_subj_p, 5)
   )
 
   if (!quietly) {
-    message(paste("Number of events (vaccine group, phase-1 subcohort):",
-                  ss$num_ph1_events_v,"\n"))
-    message(paste("Number of events (placebo group, phase-1 subcohort):",
-                  ss$num_ph1_events_p,"\n"))
-    message(paste("Number of events (vaccine group, phase-2 subcohort):",
-                  ss$num_ph2_events_v,"\n"))
-    message(paste("Number of events (placebo group, phase-2 subcohort):",
-                  ss$num_ph2_events_p,"\n"))
+
+    # Number of subjects
+    message(paste("Number of subjects (vaccine group, phase-1):",
+                  ss$num_ph1_subj_v))
+    message(paste("Number of subjects (placebo group, phase-1):",
+                  ss$num_ph1_subj_p))
+    message(paste("Number of subjects (vaccine group, phase-1):",
+                  ss$num_ph2_subj_v))
+    message(paste("Number of subjects (placebo group, phase-1):",
+                  ss$num_ph2_subj_p))
+
+    # Number of events
+    message(paste("Number of events (vaccine group, phase-1):",
+                  ss$num_ph1_events_v))
+    message(paste("Number of events (placebo group, phase-1):",
+                  ss$num_ph1_events_p))
+    message(paste("Number of events (vaccine group, phase-2):",
+                  ss$num_ph2_events_v))
+    message(paste("Number of events (placebo group, phase-2):",
+                  ss$num_ph2_events_p))
+
+    # Event proportions
+    message(paste("Proportion of subjects with an event (vaccine group, phase-",
+                  "1):", ss$prop_ph1_events_v))
+    message(paste("Proportion of subjects with an event (placebo group, phase-",
+                  "1):", ss$prop_ph1_events_p))
+    message(paste("Proportion of subjects with an event (vaccine group, phase-",
+                  "2):", ss$prop_ph2_events_v))
+    message(paste("Proportion of subjects with an event (placebo group, phase-",
+                  "2):", ss$prop_ph2_events_p))
+
   }
 
-  return(ss) # !!!!! need to return "quietly" (i.e. don't print); use `invisible`?
+  invisible(ss)
 
 }
