1f1e012d9b74b1dac4520c634b5106e321c1f314
Avi Kenny <avikenny@uw.edu>
2023-07-05 12:18:22 -0700

    Bug fix w diagnostic plots

--- R/diagnostics.R
+++ R/diagnostics.R
@@ -13,36 +13,40 @@ diagnostics <- function(obj) {
                 "t_ce()."))
   }
 
-  p1 <- ggplot2::ggplot(obj$extras$r_Mn, aes(x=s, y=est)) +
-    geom_line(color="#56B4E9") + geom_point(color="#56B4E9", size=1) +
-    labs(y="", x="log(S)", title="r_Mn")
-  p2 <- ggplot2::ggplot(obj$extras$deriv_r_Mn, aes(x=s, y=est)) +
-    geom_line(color="#56B4E9") + geom_point(color="#56B4E9", size=1) +
-    labs(y="", x="log(S)", title="deriv_r_Mn")
-  p3 <- ggplot2::ggplot(obj$extras$Gamma_os_n, aes(x=s, y=est)) +
-    geom_line(color="#56B4E9") + geom_point(color="#56B4E9", size=1) +
-    labs(y="", x="log(S)", title="Gamma_os_n")
-  p4 <- ggplot2::ggplot(obj$extras$f_s_n, aes(x=s, y=est)) +
-    geom_line(color="#56B4E9") + geom_point(color="#56B4E9", size=1) +
-    labs(y="", x="log(S)", title="f_s_n")
+  p1 <- ggplot2::ggplot(obj$extras$r_Mn, ggplot2::aes(x=s, y=est)) +
+    ggplot2::geom_line(color="#56B4E9") +
+    ggplot2::geom_point(color="#56B4E9", size=1) +
+    ggplot2::labs(y="", x="log(S)", title="r_Mn")
+  p2 <- ggplot2::ggplot(obj$extras$deriv_r_Mn, ggplot2::aes(x=s, y=est)) +
+    ggplot2::geom_line(color="#56B4E9") +
+    ggplot2::geom_point(color="#56B4E9", size=1) +
+    ggplot2::labs(y="", x="log(S)", title="deriv_r_Mn")
+  p3 <- ggplot2::ggplot(obj$extras$Gamma_os_n, ggplot2::aes(x=s, y=est)) +
+    ggplot2::geom_line(color="#56B4E9") +
+    ggplot2::geom_point(color="#56B4E9", size=1) +
+    ggplot2::labs(y="", x="log(S)", title="Gamma_os_n")
+  p4 <- ggplot2::ggplot(obj$extras$f_s_n, ggplot2::aes(x=s, y=est)) +
+    ggplot2::geom_line(color="#56B4E9") +
+    ggplot2::geom_point(color="#56B4E9", size=1) +
+    ggplot2::labs(y="", x="log(S)", title="f_s_n")
 
   p5 <- ggplot2::ggplot(
     obj$extras$Q_n,
-    aes(x=t, y=est, group=factor(interaction(ind,s)), color=factor(s))
+    ggplot2::aes(x=t, y=est, group=factor(interaction(ind,s)), color=factor(s))
   ) +
-    geom_line(alpha=0.5) +
-    facet_wrap(~s) +
-    labs(y="", title="Q_n", color="s") +
-    theme(legend.position="none")
+    ggplot2::geom_line(alpha=0.5) +
+    ggplot2::facet_wrap(~s) +
+    ggplot2::labs(y="", title="Q_n", color="s") +
+    ggplot2::theme(legend.position="none")
 
   p6 <- ggplot2::ggplot(
     obj$extras$Qc_n,
-    aes(x=t, y=est, group=factor(interaction(ind,s)), color=factor(s))
+    ggplot2::aes(x=t, y=est, group=factor(interaction(ind,s)), color=factor(s))
   ) +
-    geom_line(alpha=0.5) +
-    facet_wrap(~s) +
-    labs(y="", title="Qc_n", color="s") +
-    theme(legend.position="none")
+    ggplot2::geom_line(alpha=0.5) +
+    ggplot2::facet_wrap(~s) +
+    ggplot2::labs(y="", title="Qc_n", color="s") +
+    ggplot2::theme(legend.position="none")
 
   plot <- ggpubr::ggarrange(plotlist=list(p5,p1,p2,p6,p3,p4), ncol=3, nrow=2)
 
